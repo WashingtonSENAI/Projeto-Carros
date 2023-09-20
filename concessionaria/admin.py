7db5f6a555ae10d5258fe7889f62d7f456a793bb
Washington <wgtjunior@gmail.com>
2023-09-20 17:30:07 -0300

    Vamos construir o projeto com base nisso

---
+++ concessionaria/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Pessoa
+
+admin.site.register(Pessoa)
+
+## usuario : washington
+## senha : 123
---
+++ concessionaria/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=50)
+    idade = models.IntegerField()
+
+    def __str__(self) -> str:
+         return self.nome
+
+
+
+
+
+
+
+
+
+
+# class Veiculo(models.Model):
+#     marca = models.CharField(max_length=100)
+#     modelo = models.CharField(max_length=100)
+#     ano = models.IntegerField()
+#     preco = models.DecimalField(max_digits=10, decimal_places=2)
+#     kmrodados =  models.DecimalField(max_digits=10, decimal_places=2)	
+#     forma_de_pagamento=  models.CharField(max_length=100)
+
