655bd4b7047b7a6d1e4740073579b978ef8c82f6
Washington <wgtjunior@gmail.com>
2023-09-20 18:44:14 -0300

    MODEL mudança

--- concessionaria/models.py
+++ concessionaria/models.py
@@ -8,7 +8,18 @@ class Pessoa(models.Model):
          return self.nome
 
 
-
+class Veiculo(models.Model):
+    
+    marca = models.CharField(max_length=100)
+    modelo = models.CharField(max_length=100)
+    cor = models.CharField(max_length=100)
+    estado = models.CharField(max_length=100)
+    ano = models.IntegerField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    kmrodados =  models.DecimalField(max_digits=10, decimal_places=2)	
+    forma_de_pagamento=  models.CharField(max_length=100)
+    def __str__(self) -> str:
+         return self.marca , self.modelo      
 
 
 
