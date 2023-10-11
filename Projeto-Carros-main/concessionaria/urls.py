e2e42d75537401ddc7de88389fb5f98cfe5a2fa7
WashingtonSENAI <136704436+WashingtonSENAI@users.noreply.github.com>
2023-10-11 11:05:36 -0300

    Banco novo
    novo

---
+++ Projeto-Carros-main/concessionaria/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    
+    path('home/', views.home, name="home"),
+    path('sobrenos/', views.sobrenos, name="sobrenos"),
+    path('teste/', views.teste, name="teste"),
+    path('lista/', views.lista_teste, name='lista_pessoas'),
+    # path('veiculos/', views.listar_veiculos, name='listar_veiculos'),
+    # path('veiculos_admin/adicionar/', views.adicionar_veiculo, name='adicionar_veiculo'),
+    # path('veiculos_admin/excluir/<int:veiculo_id>/', views.excluir_veiculo, name='excluir_veiculo'),
+
+]
